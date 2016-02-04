af77c6495eebe7caf10f94236f8ecb9f070647ac
Rafagan Abreu <rafagansa@outlook.com>
2016-02-04 06:27:43 -0200

    Building first scene and getting references in script

---
+++ Assets/Scripts/HanoiAI.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HanoiAI : MonoBehaviour {
+	public GameObject bigRing, mediumRing, smallRing;
+	public Transform tower1, tower2, tower3;
+
+	private float bottomRingY, middleRingY, topRingY;
+
+	void Awake() {
+		bigRing = GameObject.Find ("BigRing");
+		mediumRing = GameObject.Find ("MediumRing");
+		smallRing = GameObject.Find ("SmallRing");
+
+		tower1 = GameObject.Find ("Tower1").transform;
+		tower2 = GameObject.Find ("Tower2").transform;
+		tower3 = GameObject.Find ("Tower3").transform;
+
+		bottomRingY = bigRing.transform.position.y;
+		middleRingY = mediumRing.transform.position.y;
+		topRingY = smallRing.transform.position.y;
+	}
+
+	// Use this for initialization
+	void Start () {
+		bigRing.transform.position = tower3.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
